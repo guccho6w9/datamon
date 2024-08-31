c1b5c65c567e5142f6dec41a6270836f360db908
guccho6w9 <gusta.quiroga033@gmail.com>
2024-08-30 23:25:28 -0300

    Add(-): updated index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,27 @@ import React, { useState } from "react";
 import '@/app/globals.css';
 import PokemonList from "@/components/Pokemon/PokemonList";
 import SearchBar from "@/components/Layout/searchbar";
+import Footer from "@/components/Layout/footer";
 
 const Home: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
 
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
+    <div className="flex flex-col min-h-screen bg-gray-100 ">
       <div className="container mx-auto max-w-4xl">
 
-        <main>
-          <div className="bg-white shadow-md rounded-lg p-6 mb-6">
+        <main className="flex-grow">
+          <div className="bg-white shadow-md rounded-lg  mt-6 mb-6">
             <SearchBar onSearch={(term) => setSearchTerm(term)} />
           </div>
 
           <PokemonList searchTerm={searchTerm} />
         </main>
+        <Footer />
       </div>
+      
     </div>
+    
   );
 };
 
