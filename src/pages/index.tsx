a995262d973dcc45bf2ed7b66fa01d08089070c5
guccho6w9 <gusta.quiroga033@gmail.com>
2024-08-31 17:19:53 -0300

    Add(-): updated index to correct footer problem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import '@/app/globals.css';
+import "@/app/globals.css";
 import PokemonList from "@/components/Pokemon/PokemonList";
 import SearchBar from "@/components/Layout/searchbar";
 import Footer from "@/components/Layout/footer";
@@ -8,21 +8,18 @@ const Home: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
 
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100 ">
-      <div className="container mx-auto max-w-4xl">
-
-        <main className="flex-grow">
-          <div className="bg-white shadow-md rounded-lg  mt-6 mb-6">
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      <div className="container mx-auto max-w-4xl flex-grow">
+        <main className="flex flex-col flex-grow">
+          <div className="bg-white shadow-md rounded-lg mt-6 mb-6">
             <SearchBar onSearch={(term) => setSearchTerm(term)} />
           </div>
 
           <PokemonList searchTerm={searchTerm} />
         </main>
-        <Footer />
       </div>
-      
+      <Footer />
     </div>
-    
   );
 };
 
